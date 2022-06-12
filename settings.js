2442fd57095069e0aab831ef60315911b217ec0d
BimsaraSan <106573559+BimsaraSan@users.noreply.github.com>
2022-06-12 18:15:51 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,20 +19,20 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
-global.footer = '©NexusNwInc.'
+global.owner = ['94710153972']
+global.premium = ['94710153972']
+global.ownernomer = '94710153972'
+global.ownername = 'RC_CHAMI'
+global.botname = '🧑‍💻RC_BOT❤️'
+global.footer = 'Crazy_Chami'
 global.ig = 'https://github.com/nexusNw'
-global.region = 'India, South India, Kerala'
+global.region = 'Srilanka, South, Galle'
 global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
-global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
-global.packname = 'Gojo-Satoru'
-global.author = '🐦Star Boy Nexus'
+global.myweb = 'he he'
+global.packname = 'Rc_hatake❤️'
+global.author = 'Rc_hatake'
 global.sessionName = 'session'
-global.prefa = ['','!','.','🐦','🐤','🗿']
+global.prefa = ['','!','.','🍂','🍃','🌹']
 global.sp = '⭔'
 global.mess = {
     success: 'Done ✓',
